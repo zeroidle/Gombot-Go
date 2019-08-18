a8e6dbe04cab18e76b4861148078396e5b22f876
Genie Chae <ygenie.chae@gmail.com>
2019-08-18 18:26:43 +0900

    test

--- gombot.go
+++ gombot.go
@@ -2,13 +2,64 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"runtime"
+	"strings"
 )
 
-var DEBUG = false
+type Config struct {
+	Proxy struct {
+		Enabled bool   `yaml:"enabled"`
+		Host    string `yaml:"host"`
+	}
+	Search Search
+}
+
+type Search struct {
+	Url string `yaml:"url"`
+}
+
+var DEBUG = true
+var config Config
 
 func main() {
 	setup()
+	yamlFile, err := ioutil.ReadFile("./.config")
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Proxy enabled: ", config.Proxy.Enabled)
+	fmt.Println("Search url: ", config.Search.Url)
+
+	if _, err := os.Stat("./.result"); os.IsNotExist(err) {
+		keyword := "도시어부"
+		search_url := strings.Replace(config.Search.Url, "%s", keyword, 1)
+		fmt.Println(search_url)
+		res, err := http.Get(search_url)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(res.StatusCode)
+		data, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", string(data))
+		fmt.Println("Connected")
+	} else {
+		result, err := ioutil.ReadFile("./.result")
+		if err != nil {
+			panic(err)
+		}
+		content := string(result)
+
+		fmt.Println("Used file")
+	}
+
 }
 
 func setup() {
